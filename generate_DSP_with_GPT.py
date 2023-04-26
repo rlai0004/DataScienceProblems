be41ec2c356cd5f12432af008adb1b302a0b8ba9
Vinh Tuan Huynh <thuynh2001.dw@gmail.com>
2023-04-26 21:42:56 +1000

    Format and remove unused line

--- generate_DSP_with_GPT.py
+++ generate_DSP_with_GPT.py
@@ -1,19 +1,17 @@
 import openai
-from dotenv import load_dotenv
 import os
 import re
 import time
 import json
-
+from dotenv import load_dotenv
 from data_science_problems.read import read_problems
 from data_science_problems.utils import write_jsonl
 
 load_dotenv()
-
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+
 def generate_code(prompt):
-    # return "def get_string(x,y):\n    return str(x)+str(y)"
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
@@ -28,42 +26,34 @@ def generate_code(prompt):
                 "content": prompt}
         ]
     )
-
     try:
         message_string = response.choices[0].message.content
-        print(message_string)
-        json_string = re.search("{(\r\n|\r|\n|.)*\"code\":(\r\n|\r|\n|.)*\"explanation\":(\r\n|\r|\n|.)*}", message_string).group()
-        print(json_string)
+        # print(message_string)
+        json_string = re.search(
+            "{(\r\n|\r|\n|.)*\"code\":(\r\n|\r|\n|.)*\"explanation\":(\r\n|\r|\n|.)*}", message_string).group()
+        # print(json_string)
         json_dict = json.loads(json_string)
         code = json_dict["code"]
-        explanation = json_dict["explanation"]
+        # explanation = json_dict["explanation"]
         return code
     except:
         return message_string
 
 
 problems = read_problems()
-
-# print(problems["DSP/0"])
-
 num_samples = 1
 samples = []
-# samples = [
-#     # dict(task_id=task_id, completion=generate_code(problems[task_id]["prompt"]))
-#     dict(task_id=task_id, prompt=problems[task_id]["prompt"], completion=generate_code(problems[task_id]["prompt"]), test=problems[task_id]["test"])
-#     for task_id in problems
-#     for _ in range(num_samples)
-# ]
 for task_id in problems:
-    if int(task_id.split("/")[1]) < 20: 
-        for _ in range(num_samples): 
+    if int(task_id.split("/")[1]) < 500:
+        for _ in range(num_samples):
             completion = ""
             try:
                 completion = generate_code(problems[task_id]["prompt"])
             except:  # handle any exceptions raised by the generate_code function
                 completion = 'Failed to extract the code snippet'
+            # samples.append(dict(task_id=task_id, prompt=problems[task_id]["prompt"], completion=completion, test=problems[task_id]["test"]))
+            # only return the task_id and the completion
             samples.append(dict(task_id=task_id, completion=completion))
-            if len(samples) % 3 == 0:  # wait for 1 minute every 3 requests
-                time.sleep(60)
-
+            # if len(samples) % 3 == 0:  # wait for 1 minute every 3 requests
+            #     time.sleep(60)
 write_jsonl("samples.jsonl", samples)
